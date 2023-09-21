ce6dd12366b1d5f5b0d2b143196cf33b94ad2b16
Modern <fadysameh10@gmail.com>
2023-09-21 14:30:25 +0300

    add custom list view item

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30.h,
+      width: 40.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        image: const DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage(AssetsData.testIMage),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:sizer/sizer.dart';
 import 'features/splash/data/presentation/views/splash_view.dart';
 
 void main() {
@@ -13,12 +13,14 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(brightness: Brightness.dark).copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        theme: ThemeData(brightness: Brightness.dark).copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashView(),
+      );
+    });
   }
 }
