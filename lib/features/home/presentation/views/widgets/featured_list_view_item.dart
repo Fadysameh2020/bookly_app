1bd18728387f0bb2109ea564de263e06fdf4d4a2
Modern <fadysameh10@gmail.com>
2023-09-22 10:31:46 +0300

    best seller item done

--- lib/features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,20 +1,20 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 
 class FeaturedListViewItem extends StatelessWidget {
   const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 30.h,
-      width: 40.w,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: const DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage(AssetsData.testIMage),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testIMage),
+          ),
         ),
       ),
     );
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -16,12 +17,16 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(icon: Icons.search),
           FeaturedBooksListView(),
           SizedBox(
-            height: 40,
+            height: 24,
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
     );
